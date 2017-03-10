3f70a71d9031ad56930f83a5b9c1a58797481d13
kristjanjansen <kristjan.jansen@gmail.com>
2017-03-10 16:08:46 +0200

    ok

--- scripts/generate_cities2.js
+++ scripts/generate_cities2.js
@@ -3,6 +3,7 @@ var async = require('async');
 var _ = require('lodash');
 
 var destinations = require('./destinations.json')
+var destinations_noresult = require('./destinations_noresult.json')
 
 var citiesData = []
 
@@ -17,11 +18,14 @@ var citiesData = []
     
     async.each(cities, (city, cb) => {
         var cityData = {}
+        var id = destinations_noresult.find(result => result.id === city.id)
+        var url = !!id
+            ? 'http://api.geonames.org/getJSON?username=kristjanjansen&geonameId=' + id.geonameId
+            : 'http://api.geonames.org/searchJSON?formatted=true&q='+city.name+'&maxRows=1&username=kristjanjansen&style=full&lang=et'
         request(
-            { url: 'http://api.geonames.org/searchJSON?formatted=true&q='+city.name+'&maxRows=1&username=kristjanjansen&style=full&lang=et', json: true},
+            { url, json: true},
             (err, res, body) => {
-                if (body.totalResultsCount > 0) {
-                    var data = body.geonames[0]
+                    var data = !!id ? body : body.geonames[0]
                     cityData = {
                         id: city.id,
                         name: city.name,
@@ -31,31 +35,18 @@ var citiesData = []
                         geonameId: data.geonameId,
                         population: data.population
                     }
-                } else {
-                    // http://api.geonames.org/getJSON?geonameId=2622320&username=kristjanjansen
-                    cityData = {
-                        id: city.id,
-                        name: city.name,
-                        //lat: 0,
-                        //lon: 0
-                        geonameId: 0
-                    }
-                }
+
                 citiesData.push(cityData)
                 setTimeout(cb, 50)
             }
         )
     }, () => {
-        noresult = _.sortBy(citiesData, 'id')
-            .filter(city => city.geonameId == 0)
-        console.log(JSON.stringify(noresult, null, 4))
-
-        /*console.log('\n<?php\n\nreturn [\n')
-        _.sortBy(citiesData, 'id')
-            .filter(city => city.geonameId !== 0)
-            .forEach(city => {
     
-                console.log(`    ${city.id} => [
+    console.log('\n<?php\n\nreturn [\n')
+        _.sortBy(citiesData, 'id')
+        .forEach(city => {
+    
+    console.log(`    ${city.id} => [
         'name' => "${city.name}",
         'lat' => ${city.lat},
         'lon' => ${city.lon},
@@ -63,11 +54,11 @@ var citiesData = []
         'population' => ${city.population},
         'geoname' => ${city.geonameId},
     ],`);
-            
-            })
-    
-        console.log('\n];\n')
-        */
+        
+        })
+
+    console.log('\n];\n')
+        
     })
 
 /*
